5eb5e3f1c62b520ac71d424f3e9ad55a28679a9c
dicky375 <danieldickson601@gmail.com>
2026-07-22 11:30:25 +0100

    deleted

--- servers/server2-registration/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-export const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// Root .env is 2 levels up from servers/server2-registration/
-dotenv.config({ path: resolve(__dirname, '../../.env') });
